2dad50477bb32d5288c48e745e3722ff7a4d3ed9
dustytrash <Lionelbergen@live.com>
2024-09-08 12:35:01 -0500

    Added logging library

--- src/demo.js
+++ src/demo.js
@@ -1,8 +1,8 @@
+import log from './util/log.js';
 import { getPostsFromSubreddit, getSubredditModList, getLatestCommentsFromReddit} from './RedditClient.js';
 
 // const postsList = await getPostsFromSubreddit(1, 'test', 'new');
 // const modList = await getSubredditModList('test');
 const commentsList = await getLatestCommentsFromReddit(1000);
 
-// console.log(commentsList);
-console.log(commentsList.length);
+log.info(commentsList);
