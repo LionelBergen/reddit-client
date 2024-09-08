270a13485b78457db674e206f922d373bf016274
dustytrash <Lionelbergen@live.com>
2024-09-08 13:02:22 -0500

    got eslint working with module, and ran auto fix

--- src/util/reddit/reddit-parser.js
+++ src/util/reddit/reddit-parser.js
@@ -1,19 +1,19 @@
 export function parsePostArray(rawResponse) {
   const data = rawResponse.data || rawResponse;
 
-return {
-  body: data.selftext,
-  subreddit: data.subreddit,
-  authorFullname: data.author_fullname,
-  postTitle: data.title,
-  name: data.name,
-  ups: data.ups,
-  score: data.score,
-  created: data.created_utc,
-  id: data.id,
-  author: data.author,
-  permalink: data.permalink,
-  url: 'https://www.reddit.com' + data.permalink
+  return {
+    body: data.selftext,
+    subreddit: data.subreddit,
+    authorFullname: data.author_fullname,
+    postTitle: data.title,
+    name: data.name,
+    ups: data.ups,
+    score: data.score,
+    created: data.created_utc,
+    id: data.id,
+    author: data.author,
+    permalink: data.permalink,
+    url: 'https://www.reddit.com' + data.permalink
   };
 }
 
