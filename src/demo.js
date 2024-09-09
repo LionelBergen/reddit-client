286ca1b11ccb54ece285d7b2ec9b3fa359ea16fc
dustytrash <Lionelbergen@live.com>
2024-09-08 19:56:41 -0500

    eslint no space before parenthesis

--- src/demo.js
+++ src/demo.js
@@ -2,7 +2,6 @@ import log from './util/log.js';
 import { getLatestCommentsFromReddit } from './RedditClient.js';
 
 (async () => {
-
   // log.setLevel('debug');
   // const postsList = await getPostsFromSubreddit(1, 'test', 'new');
   // const modList = await getSubredditModList('test');
