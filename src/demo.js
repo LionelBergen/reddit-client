5e971fd235179d67e89a1b4134058dd82524f528
dustytrash <Lionelbergen@live.com>
2024-09-29 16:15:58 -0500

    added .env file for demo file

--- src/demo.js
+++ src/demo.js
@@ -1,10 +1,16 @@
 import log from './util/log.js';
 import { CreateAuthedClient } from './reddit-client.js';
 import { getEnvironmentVariable } from './util/util.js';
+import 'dotenv/config';
 
 const DEFAULT_USER_AGENT = 'u/dusty-trash reddit-simple-client/2.0.0 by Lionel Bergen';
 
 function isValidAuthModal(authModal) {
+  for (const property in authModal) {
+    if (!authModal[property]) {
+      log.error(`${property} is not set!`);
+    }
+  }
   return authModal.username && authModal.password && authModal.appId && authModal.appSecret;
 }
 
@@ -27,12 +33,12 @@ function isValidAuthModal(authModal) {
 
   try {
     const redditClient = await CreateAuthedClient({ redditOptions: redditOptions, redditAuth: redditAuthModal });
-    const results = await redditClient.getPostsFromSubreddit({
+    /* const results = await redditClient.getPostsFromSubreddit({
       numberOfPosts: 1000,
       subreddit: 'test',
       sortType: 'new'
-    });
-    // const results = await redditClient.getSubredditModList('test');
+    }); */
+    const results = await redditClient.getSubredditModList('test');
     // const results = await redditClient.postComment('t1_lnbe9pa', 'testing, written from node.js');
     log.info(results);
     log.info(results.length);
